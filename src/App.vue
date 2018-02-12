38890034caa998fdc7bde36db59e0972aad48829
Lan <alias8516@sina.com.cn>
2018-02-12 18:26:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<header>HEY UI</header>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="less">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		header {
+			background: rgb(251, 21, 30);
+		}
+	}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="hello">
+		<Button @click="show=true">开始第一个demo</Button>
+		<Modal v-model="show">
+			<div>欢迎开始heyui之旅~</div>
+		</Modal>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {
+			show: false,
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+
+</style>
