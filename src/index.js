9c4adde142f149b10507a639ee204de255e91327
vvpvvp <alias8516@sina.com.cn>
2018-04-16 14:05:08 +0800

    use css

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import initDict from './js/config/dict-config';
 initDict(HeyUI);
 Vue.use(HeyUI);
 
-require("heyui/themes/index.less");
+Vue.config.productionTip = false;
+require("heyui/build/index.css");
+
 new Vue({
   el: "#app",
   router,
