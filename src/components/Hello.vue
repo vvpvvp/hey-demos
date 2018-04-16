45e39362c04118b904e5403f05257c96cc0f5077
vvpvvp <alias8516@sina.com.cn>
2018-04-16 14:27:26 +0800

    change hello

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,7 +4,9 @@
 		<Modal v-model="show">
 			<div>欢迎开始heyui之旅~</div>
 		</Modal>
+
 		<div v-height="40"></div>
+		
 		<div>
 			<h2>使用datas定义数据来源</h2>
 			<p><router-link :to="{name:'datasDemo1'}">demo1: 数据驱动-key单选</router-link></p>
